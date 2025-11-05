6677e54dc847bc9899f733461da346816caada48
rafael.resende <rafael.resende@playbrandsgroup.com>
2025-11-05 00:30:59 -0300

    LTabNavigation fixed

--- Runtime/LocalizadedWidgets/Scripts/LTabNavigation.cs
+++ Runtime/LocalizadedWidgets/Scripts/LTabNavigation.cs
@@ -1,4 +1,5 @@
 using Concept.Localization;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SocialPlatforms;
@@ -17,14 +18,13 @@ namespace Concept.UI
         [UxmlAttribute("collection")]
         public string collection { get; set; } = "UI";
 
-
-        public LTabNavigation() 
+        public LTabNavigation()
         {
+
             RegisterCallback<AttachToPanelEvent>(evt =>
             {
-                _ = UpdateTabButtonsAsync();
-                SelectIndex(index);
-                LocalizationProvider.RegisterUpdateAction(UpdateTabButtonsAsync);
+                    LocalizationProvider.RegisterUpdateAction(UpdateTabButtonsAsync);
+                    SelectIndex(index);
             });
 
 
@@ -47,16 +47,19 @@ namespace Concept.UI
         private async Task UpdateTabButtonsAsync()
         {
 
-            Clear();
-
-
+            List<string> captions = new List<string>();
             for (int i = 0; i < m_tabButtons.Length; i++)
             {
                 string key = m_tabButtons[i];
                 var (success, text) = await LocalizationProvider.GetLocalizedStringAsync(collection, key);
+                captions.Add(success ? text : key);
+            }
+            Clear();
+            for (int i = 0; i < captions.Count; i++)
+            {
                 int id = i;
                 var tabBt = new Button();
-                tabBt.text = success ? text : key;
+                tabBt.text = captions[i];
                 tabBt.clicked += () => SelectIndex(id);
                 tabBt.AddToClassList("tab-button");
                 if (i == index)
